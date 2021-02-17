09bdbebeeb7849dcfb2f6410a5104a1431c9dca6
Thomas Pietschmann <thomas@pietschi.com>
2021-02-17 22:06:25 +0100

    model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lenNon")
+    assert_equal "John Lennon", user.full_name
+  end
 end
